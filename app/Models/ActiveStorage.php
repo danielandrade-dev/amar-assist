663bf2843e32fdbf72627d6997d6205757bc8b04
Daniel M. Andrade <danielthooth@gmail.com>
2025-04-15 14:01:10 -0300

    Adicionando modelos ActiveStorage e Product, além de migrações para as tabelas active_storages e products, incluindo suporte a soft deletes.

---
+++ app/Models/ActiveStorage.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
+class ActiveStorage extends Model
+{
+    use HasFactory;
+    protected $fillable = ['filename', 'uuid', 'service_name', 'content_type', 'product_id'];
+    protected $table = 'active_storages';
+
+    public static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            $model->uuid = Str::uuid();
+            $model->service_name = config('filesystems.default');
+            $model->content_type = $model->getContentType();
+            $model->product_id = $model->product_id ?? null;
+        });
+    }
+    public function Products()
+    {
+        return $this->hasMany(Product::class, 'product_id', 'id');
+    }
+}
--- database/migrations/2025_04_15_164657_create_products_table.php
+++ database/migrations/2025_04_15_164657_create_products_table.php
@@ -13,13 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('description');
+            $table->decimal('price', 8, 2);
+            $table->integer('price_original');
+            $table->integer('quantity');
+            $table->boolean('status');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -32,6 +33,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
