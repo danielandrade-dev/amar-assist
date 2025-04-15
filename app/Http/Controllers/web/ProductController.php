975c56a8739d9e910274764cf28c8b5ec1c36b11
Daniel M. Andrade <danielthooth@gmail.com>
2025-04-15 16:15:34 -0300

    Implementando operações CRUD no ProductController e UserController, incluindo métodos para listar, criar, editar e deletar produtos e usuários, além de integrar requisições com validações específicas.

--- app/Http/Controllers/web/ProductController.php
+++ app/Http/Controllers/web/ProductController.php
@@ -3,9 +3,36 @@
 namespace App\Http\Controllers\web;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-
+use App\Models\Product;
+use App\Http\Requests\ProductRequest;
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = Product::all()->with('activeStorage');
+        return inertia('products/index', compact('products'));
+    }
+    public function create()
+    {
+        return inertia('products/create');
+    }
+    public function store(ProductRequest $request)
+    {
+        Product::create($request->all());
+        return redirect()->route('products.index')->with('success', 'Produto criado com sucesso');
+    }
+    public function edit(Product $product)
+    {
+        return inertia('products/edit', compact('product'));
+    }
+    public function update(ProductRequest $request, Product $product)
+    {
+        $product->update($request->all());
+        return redirect()->route('products.index')->with('success', 'Produto atualizado com sucesso');
+    }
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return redirect()->route('products.index')->with('success', 'Produto deletado com sucesso');
+    }
 }
--- app/Http/Controllers/web/UserController.php
+++ app/Http/Controllers/web/UserController.php
@@ -4,8 +4,38 @@ namespace App\Http\Controllers\web;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\User;
+use App\Http\Requests\ProfileUpdateRequest;
+use Inertia\Inertia;
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        $users = User::all();
+        return inertia::render('users/index', compact('users'));
+    }
+    public function create()
+    {
+        return inertia::render('users/create');
+    }
+    public function store(Request $request)
+    {
+        $user = User::create($request->all());
+        return redirect()->route('users.index')->with('success', 'Usuário criado com sucesso');
+    }
+    public function edit(User $user)
+    {
+        return inertia::render('users/edit', compact('user'));
+    }
+    public function update(ProfileUpdateRequest $request, User $user)
+    {
+        $user->update($request->all());
+        return redirect()->route('users.index')->with('success', 'Usuário atualizado com sucesso');
+    }
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return redirect()->route('users.index')->with('success', 'Usuário deletado com sucesso');
+    }
 }
+
